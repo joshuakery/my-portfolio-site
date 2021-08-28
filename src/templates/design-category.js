f4c18935b708fb4f2995092c0912526d3a207bb8
Josh Kery <joshuakery1@gmail.com>
2021-08-28 14:09:01 -0400

    fixed build error for design category page

--- src/templates/design-category.js
+++ src/templates/design-category.js
@@ -2,105 +2,10 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Link, graphql } from "gatsby"
 import catStyles from "../templates/design-category.module.css"
-import Img from "gatsby-image"
-import {Bathroom, Bedroom, Kitchen, Basement} from "../components/specialRoom"
+import Image from "gatsby-dynamic-image"
 
 import { Helmet } from "react-helmet"
 import { DesignLayout } from "../components/layout";
-
-const getRooms = (edges, location) => {
-  let rooms = [];
-  for (let i=0; i < edges.length-1; i++) {
-    let {node} = edges[i];
-    const { slug } = node.fields
-    const { title, featuredImage } = node.frontmatter
-    rooms.push (
-        <li key={slug} className={catStyles.room_container}>
-          <Link to={slug} state={{previousPage: location.pathname}}>
-
-            <Img fluid={featuredImage.childImageSharp.fluid}
-                            className={catStyles.featuredImage}/>
-
-            <div className={catStyles.room_link_container}>
-                <div className={catStyles.room_link_background}>
-                    <p className={catStyles.room_link}>
-                        {title}
-                    </p>
-                </div>
-            </div>
-
-            <div className={catStyles.left_brick_hole}></div>
-            <div className={catStyles.right_brick_hole}></div>
-
-          </Link>
-        </li>
-    )
-  }
-  return rooms
-}
-
-const getLastRoom = (edges,location) => {
-  let {node} = edges[edges.length-1];
-  const { slug } = node.fields
-  const { title, featuredImage } = node.frontmatter
-  return (
-      <li key={slug} className={catStyles.basement_container}>
-        <Link to={slug} state={{previousPage: location.pathname}}>
-
-          <Img fluid={featuredImage.childImageSharp.fluid}
-                          className={catStyles.featuredImage}/>
-
-          <div className={catStyles.room_link_container}>
-              <div className={catStyles.room_link_background}>
-                  <p className={catStyles.room_link}>
-                      {title}
-                  </p>
-              </div>
-          </div>
-
-        </Link>
-      </li>
-  )
-}
-
-const getSpecialRoom = (cat) => {
-  if (cat == "2d") {
-    return (<Bedroom></Bedroom>)
-  }
-  else if (cat == "3d") {
-    return (<Bathroom></Bathroom>)
-  }
-  else if (cat == "Media") {
-    return (<Kitchen></Kitchen>)
-  }
-  else if (cat == "Time-Based") {
-    return (<Basement></Basement>)
-  }
-  else {
-    return null;
-  }
-}
-
-//SAVING THIS BECAUSE THIS IS THE OLD CATS, FOR THE OLD ART WEBSITE
-// const Cats = ({ pageContext, data, location }) => {
-//   const { category } = pageContext
-//   const { edges } = data.allMarkdownRemark
-
-//   return (
-//     <Layout>
-//         <div className={catStyles.container}>
-//           <h1>{category}</h1>
-//           <House2>
-//             {/* {getSpecialRoom(category)} */}
-//             {getRooms(edges, location)}
-//           </House2>
-//           <Ground2>
-//             {getLastRoom(edges,location)}
-//           </Ground2>
-//         </div>
-//     </Layout>
-//   )
-// }
 
 const getProjects = (edges) => {
   let projects = [];
@@ -108,10 +13,11 @@ const getProjects = (edges) => {
       let {node} = edges[i];
       const { slug } = node.fields;
       const { title, featuredImage } = node.frontmatter;
+      console.log(featuredImage);
       projects.push(
         <li key={slug}>
             <Link to={slug}>
-                <Img fluid={featuredImage.childImageSharp.fluid}/>
+                <Image node={featuredImage} className={catStyles.featuredImage}/>
                 <h3>{title}</h3>
             </Link>
         </li>
@@ -119,24 +25,6 @@ const getProjects = (edges) => {
   }
   return projects
 }
-
-// const Cats = ({ pageContext, data, location }) => {
-//   const { category } = pageContext;
-//   const { edges } = data.allMarkdownRemark;
-//   return (
-//     <DesignLayout>
-//         <div>
-//           <Helmet title={category + " | Joshua Kery"} />
-//           <div>
-//             <h1 className={catStyles.catTitle}>{category}</h1>
-//             <ul className={catStyles.projectsList}>
-//               {getProjects(edges)}
-//             </ul>
-//           </div>
-//         </div>
-//     </DesignLayout>
-//   );
-// }
 
 const DESIGNCATS = ['teaching','live-shows','design','creative-coding'];
 const Cats = ({ pageContext, data, location }) => {
@@ -155,36 +43,6 @@ const Cats = ({ pageContext, data, location }) => {
             </div>
         </DesignLayout>
       );
-  // if (category in DESIGNCATS) {
-  //   return (
-  //     <DesignLayout>
-  //         <div>
-  //           <Helmet title={category + " | Joshua Kery"} />
-  //           <div>
-  //             <h1 className={catStyles.catTitle}>{category}</h1>
-  //             <ul className={catStyles.projectsList}>
-  //               {getProjects(edges)}
-  //             </ul>
-  //           </div>
-  //         </div>
-  //     </DesignLayout>
-  //   );
-  // } else {
-  //   return (
-  //     <Layout>
-  //         <div className={catStyles.container}>
-  //           <h1>{category}</h1>
-  //           <House2>
-  //             {/* {getSpecialRoom(category)} */}
-  //             {getRooms(edges, location)}
-  //           </House2>
-  //           <Ground2>
-  //             {getLastRoom(edges,location)}
-  //           </Ground2>
-  //         </div>
-  //     </Layout>
-  //   );
-  // }
 }
 
 Cats.propTypes = {
