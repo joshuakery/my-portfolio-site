14047f67879d5aaa7c12d57bdc46a85cc70d1111
Josh Kery <joshuakery1@gmail.com>
2021-08-28 14:57:42 -0400

    fixed bug with category js

--- src/templates/category.js
+++ src/templates/category.js
@@ -7,11 +7,9 @@ import Basement from "../components/room_basement"
 import Ground from "../components/ground"
 import { Link, graphql } from "gatsby"
 import catStyles from "../templates/category.module.css"
-import Img from "gatsby-image"
-// import {Bathroom, Bedroom, Kitchen, Basement} from "../components/specialRoom"
+import Image from "gatsby-dynamic-image"
 
 const getRooms = (edges, location) => {
-  // console.log(location);
   let rooms = [];
   for (let i=0; i < edges.length-1; i++) {
     let {node} = edges[i];
@@ -20,29 +18,10 @@ const getRooms = (edges, location) => {
     rooms.push (
         <li key={slug} className={catStyles.room_container}>
           <Room title={title} slug={slug}>
-            <Img fluid={featuredImage.childImageSharp.fluid}
+            <Image node={featuredImage}
                   className={catStyles.featuredImage}/>
           </Room>
         </li>
-        // 
-        //   <Link to={slug} state={{previousPage: location.pathname}}>
-
-        //     <Img fluid={featuredImage.childImageSharp.fluid}
-        //                     className={catStyles.featuredImage}/>
-
-        //     <div className={catStyles.room_link_container}>
-        //         <div className={catStyles.room_link_background}>
-        //             <p className={catStyles.room_link}>
-        //                 {title}
-        //             </p>
-        //         </div>
-        //     </div>
-
-        //     <div className={catStyles.left_brick_hole}></div>
-        //     <div className={catStyles.right_brick_hole}></div>
-
-        //   </Link>
-        // </li>
     )
   }
   return rooms
@@ -55,44 +34,12 @@ const getLastRoom = (edges,location) => {
   return (
       <li key={slug} className={catStyles.basement_container}>
         <Basement title={title} slug={slug}>
-          <Img fluid={featuredImage.childImageSharp.fluid}
+          <Image node={featuredImage}
                className={catStyles.featuredImage}/>
         </Basement>
-        {/* <Link to={slug} state={{previousPage: location.pathname}}>
-
-          <Img fluid={featuredImage.childImageSharp.fluid}
-                          className={catStyles.featuredImage}/>
-
-          <div className={catStyles.room_link_container}>
-              <div className={catStyles.room_link_background}>
-                  <p className={catStyles.room_link}>
-                      {title}
-                  </p>
-              </div>
-          </div>
-
-        </Link> */}
       </li>
   )
 }
-
-// const getSpecialRoom = (cat) => {
-//   if (cat == "2d") {
-//     return (<Bedroom></Bedroom>)
-//   }
-//   else if (cat == "3d") {
-//     return (<Bathroom></Bathroom>)
-//   }
-//   else if (cat == "Media") {
-//     return (<Kitchen></Kitchen>)
-//   }
-//   else if (cat == "Time-Based") {
-//     return (<Basement></Basement>)
-//   }
-//   else {
-//     return null;
-//   }
-// }
 
 //SAVING THIS BECAUSE THIS IS THE OLD CATS, FOR THE OLD ART WEBSITE
 const Cats = ({ pageContext, data, location }) => {
@@ -113,24 +60,6 @@ const Cats = ({ pageContext, data, location }) => {
         </div>
     </Layout>
   )
-}
-
-const getProjects = (edges) => {
-  let projects = [];
-  for (let i=0; i < edges.length; i++) {
-      let {node} = edges[i];
-      const { slug } = node.fields;
-      const { title, featuredImage } = node.frontmatter;
-      projects.push(
-        <li key={slug}>
-            <Link to={slug}>
-                <Img fluid={featuredImage.childImageSharp.fluid}/>
-                <h3>{title}</h3>
-            </Link>
-        </li>
-      )
-  }
-  return projects
 }
 
 Cats.propTypes = {
