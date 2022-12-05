948a0c1327b1c9d9be42691a22298c7b2860d1bc
Josh Kery <joshuakery1@gmail.com>
2022-12-05 12:56:35 -0500

    test change

--- src/components/design/intro.js
+++ src/components/design/intro.js
@@ -6,7 +6,7 @@ export default ({children}) => {
     return (
         <div className={introStyles.frame}>
             <p className={introStyles.text}>
-                I create learning materials, visitor interfaces, and tools for digital performances.
+                I create learning materials, user interfaces, and tools for digital performances.
             </p>
             <p className={introStyles.text}>
                 I currently work as a freelance software developer, frequently for <a href="https://rlmg.com/" target="_blank">Richard Lewis Media Group.</a>
